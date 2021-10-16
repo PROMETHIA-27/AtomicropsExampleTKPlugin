9bbfdbe3aa4d2bb67c4e319cecfe10de5fe1ab1d
PROMETHIA <electriccobras@gmail.com>
2021-10-16 16:59:08 -0400

    Initial Commit

---
+++ Assets/Scripts/Plugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+
+namespace ExampleTKPlugin
+{
+    [BepInPlugin("com.PROMETHIA.ExampleTKPlugin", "Example TK Plugin", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static BepInEx.Logging.ManualLogSource _log;
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            this.Logger.LogInfo("Plugin com.PROMETHIA.ExampleTKPlugin is loaded!");
+            _log = this.Logger;
+        }
+    }
+}
+
